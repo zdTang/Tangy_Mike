238f9299dc674b5f1c87b71efd024105add62c24
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2023-06-03 19:40:58 -0400

    3-5 Dropdown and data binding

--- Tangy_Models/LearnBlazorModels/Demo_Product.cs
+++ Tangy_Models/LearnBlazorModels/Demo_Product.cs
@@ -6,5 +6,7 @@
         public string Name { get; set; }
         public double Price { get; set; }
         public bool IsActive { get; set; }
+
+        public IEnumerable<Demo_ProductProp> ProductProperties { get; set; }
     }
 }
